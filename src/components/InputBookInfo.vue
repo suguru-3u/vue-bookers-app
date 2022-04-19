71f1e3ce28a2655b47c9a260ff22347d18002645
suguru-3u <suguru.13579515@gmail.com>
2022-04-20 06:37:51 +0900

    #1 本の投稿機能の完成、データも確認済み

---
+++ src/components/InputBookInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>Book</h2>
+    <label for="title">Title</label>
+    <input type="text" v-model="bookTitle" /><br />
+    <label for="thought">Thooughts</label>
+    <textarea cols="60" rows="10" v-model="bookThoughts"></textarea>
+    <button v-on:click="addBookInfo">create</button><br />
+    {{ $data }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+type bookInfo = {
+  bookTitle: string;
+  bookThoughts: string;
+  bookInfoArray: bookInfoObj[];
+};
+
+type bookInfoObj = {
+  title: string;
+  thooughts: string;
+};
+
+export default defineComponent({
+  name: "InputBookInfo",
+  data(): bookInfo {
+    return {
+      bookTitle: "",
+      bookThoughts: "",
+      bookInfoArray: [],
+    };
+  },
+  methods: {
+    addBookInfo() {
+      this.bookInfoArray.push({
+        title: this.bookTitle,
+        thooughts: this.bookThoughts,
+      });
+      this.bookTitle = "";
+      this.bookThoughts = "";
+    },
+  },
+});
+</script>
