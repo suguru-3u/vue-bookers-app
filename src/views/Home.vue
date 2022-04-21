dde363c4d7b1b841a1fbfde93b8b7e13a261a0f0
suguru-3u <suguru.13579515@gmail.com>
2022-04-21 09:15:44 +0900

    #3 Introducing bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <InputBookInfo />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import InputBookInfo from "@/components/InputBookInfo.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    InputBookInfo,
   },
 });
 </script>
