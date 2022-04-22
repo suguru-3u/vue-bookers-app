15c5b62e1d6d5e0edd1299ccbc8a7c7a75739925
suguru-3u <suguru.13579515@gmail.com>
2022-04-23 07:58:07 +0900

    #9 バリデーション機能の実装

--- src/components/InputBookInfo.vue
+++ src/components/InputBookInfo.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h2>Book</h2>
-    <form action="">
+    <form>
       <div class="md-4">
         <label for="title" class="form-label">Title</label>
         <input type="text" v-model="bookTitle" class="form-control" />
+        <p v-if="errorTitleFlg" class="text-danger">空白では登録できません</p>
       </div>
       <div class="md-4 mt-3 mb-5">
         <label for="thought" class="form-label">Thooughts</label>
         <textarea v-model="bookThoughts" class="form-control"></textarea>
+        <p v-if="errorThoughtsFlg" class="text-danger">
+          空白では登録できません
+        </p>
       </div>
       <button type="button" class="btn btn-success" v-on:click="addBookInfo">
         create
@@ -23,6 +27,8 @@ import { defineComponent } from "vue";
 type bookInfo = {
   bookTitle: string;
   bookThoughts: string;
+  errorTitleFlg: boolean;
+  errorThoughtsFlg: boolean;
 };
 
 export default defineComponent({
@@ -31,13 +37,21 @@ export default defineComponent({
     return {
       bookTitle: "",
       bookThoughts: "",
+      errorTitleFlg: false,
+      errorThoughtsFlg: false,
     };
   },
   methods: {
     addBookInfo() {
-      this.$emit("addBookInfo", this.bookTitle, this.bookThoughts);
-      this.bookTitle = "";
-      this.bookThoughts = "";
+      this.errorTitleFlg = false;
+      this.errorThoughtsFlg = false;
+      if (!this.bookTitle.trim()) this.errorTitleFlg = true;
+      if (!this.bookThoughts.trim()) this.errorThoughtsFlg = true;
+      if (this.errorTitleFlg === false && this.errorThoughtsFlg === false) {
+        this.$emit("addBookInfo", this.bookTitle, this.bookThoughts);
+        this.bookTitle = "";
+        this.bookThoughts = "";
+      }
     },
   },
 });
