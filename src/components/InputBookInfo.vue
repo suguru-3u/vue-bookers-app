5b7892d28a2617a45516b41ef27fea1543e034a6
suguru-3u <suguru.13579515@gmail.com>
2022-04-22 09:23:22 +0900

    #7 Design modification

--- src/components/InputBookInfo.vue
+++ src/components/InputBookInfo.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    {{ $data }}
     <h2>Book</h2>
-    <label for="title">Title</label>
-    <input type="text" v-model="bookTitle" /><br />
-    <label for="thought">Thooughts</label>
-    <textarea cols="60" rows="10" v-model="bookThoughts"></textarea><br />
-    <button type="button" class="btn btn-success" v-on:click="addBookInfo">
-      create</button
-    ><br />
+    <form action="">
+      <div class="md-4">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" v-model="bookTitle" class="form-control" />
+      </div>
+      <div class="md-4 mt-3 mb-5">
+        <label for="thought" class="form-label">Thooughts</label>
+        <textarea v-model="bookThoughts" class="form-control"></textarea>
+      </div>
+      <button type="button" class="btn btn-success" v-on:click="addBookInfo">
+        create
+      </button>
+    </form>
   </div>
 </template>
 
@@ -18,13 +23,7 @@ import { defineComponent } from "vue";
 type bookInfo = {
   bookTitle: string;
   bookThoughts: string;
-  // bookInfoArray: bookInfoObj[];
 };
-
-// type bookInfoObj = {
-//   title: string;
-//   thooughts: string;
-// };
 
 export default defineComponent({
   name: "InputBookInfo",
@@ -32,7 +31,6 @@ export default defineComponent({
     return {
       bookTitle: "",
       bookThoughts: "",
-      // bookInfoArray: [],
     };
   },
   methods: {
